33f9848070a9e894ccc74259c09f03bba19f4aad
Daniel Du <duchangyu@gmail.com>
2016-02-01 16:26:27 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# meteor-view.and.data.api
+------
+
+This samples demos how to embed a viewer into a Meteor application.
+
+Please refer to this blog post for detail: 
+http://adndevblog.typepad.com/cloud_and_mobile/2016/01/using-view-and-data-api-with-meteor.html 
+http://adndevblog.typepad.com/cloud_and_mobile/2016/01/using-view-and-data-api-with-meteorpart-2.html
+
---
+++ server/index.js
@@ -0,0 +1,39 @@
+Meteor.startup(function () {
+    // code to run on server at startup
+});
+
+
+Meteor.methods({
+
+    getAccessToken: function () {
+
+        this.unblock();
+
+        var credentials = {
+
+            credentials: {
+                // Replace placeholder below by the Consumer Key and Consumer Secret you got from
+                // http://developer.autodesk.com/ for the production server
+                client_id: process.env.CONSUMERKEY || '<your consumer key>',
+                client_secret: process.env.CONSUMERSECRET || '<your secret key>',
+                grant_type: 'client_credentials'
+            },
+
+            // If you which to use the Autodesk View & Data API on the staging server, change this url
+            BaseUrl: 'https://developer.api.autodesk.com',
+            Version: 'v1'
+        };
+
+        credentials.AuthenticationURL = credentials.BaseUrl + '/authentication/' + credentials.Version + '/authenticate'
+
+        //must use synchronous mode
+        var result = Meteor.http.post(
+            credentials.AuthenticationURL,
+            {params: credentials.credentials}
+        );
+        //get the access token object
+        return result.data;
+
+
+    }
+})
