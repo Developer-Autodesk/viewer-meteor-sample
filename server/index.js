7ecc6e6196bce86dfcf666c766c05e311eb97d3f
Augusto Goncalves <augusto.goncalves@autodesk.com>
2016-11-07 11:18:26 -0200

    adjust variable names

--- server/index.js
+++ server/index.js
@@ -14,9 +14,10 @@ Meteor.methods({
             credentials: {
                 // Replace placeholder below by the Consumer Key and Consumer Secret you got from
                 // http://developer.autodesk.com/ for the production server
-                client_id: process.env.CONSUMERKEY || '<your consumer key>',
-                client_secret: process.env.CONSUMERSECRET || '<your secret key>',
-                grant_type: 'client_credentials'
+                client_id: process.env.FORGE_CLIENT_ID || '<your consumer key>',
+                client_secret: process.env.FORGE_CLIENT_SECRET || '<your secret key>',
+                grant_type: 'client_credentials',
+                scope: 'data:read'
             },
 
             // If you want to use the Autodesk View & Data API on the staging server, change this url to
